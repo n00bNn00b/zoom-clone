33573fc8112b73113d0c8c7e23c14ec102d79256
n00bNn00b <reldewan9@gmail.com>
2022-07-29 20:22:10 +0600

    socket.io-client installed

---
+++ components/StartMeeting.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+function StartMeeting({ name, roomId, setName, setRoomId }) {
+  return (
+    <View style={styles.startMeetingContainer}>
+      <View style={styles.info}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Enter Name"
+          placeholderTextColor="#767476"
+          value={name}
+          onChangeText={(text) => setName(text)}
+        />
+      </View>
+      <View style={styles.info}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Enter Room ID"
+          placeholderTextColor="#767476"
+          value={name}
+          onChangeText={(text) => setRoomId(text)}
+        />
+      </View>
+      <View style={{ alignItems: "center" }}>
+        <TouchableOpacity style={styles.startMeetingButton}>
+          <Text style={{ color: "white", fontWeight: "bold" }}>
+            Start Meeting
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+export default StartMeeting;
+
+const styles = StyleSheet.create({
+  info: {
+    width: "100%",
+    backgroundColor: "#373538",
+    height: 50,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: "#484648",
+    padding: 12,
+    justifyContent: "center",
+  },
+  textInput: {
+    color: "white",
+    fontSize: 18,
+  },
+  startMeetingButton: {
+    width: 350,
+    height: 50,
+    marginTop: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#0470DC",
+    borderRadius: 15,
+  },
+});
--- screens/MeetingRoom.js
+++ screens/MeetingRoom.js
@@ -1,45 +1,22 @@
-import React, { useState } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  TextInput,
-  Touchable,
-  TouchableOpacity,
-} from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+
+import StartMeeting from "../components/StartMeeting";
 
 function MeetingRoom() {
   const [name, setName] = useState("");
   const [roomId, setRoomId] = useState();
+  useEffect(() => {
+    console.log("working");
+  }, []);
   return (
     <View style={styles.container}>
-      <View style={styles.startMeetingContainer}>
-        <View style={styles.info}>
-          <TextInput
-            style={styles.textInput}
-            placeholder="Enter Name"
-            placeholderTextColor="#767476"
-            value={name}
-            onChangeText={(text) => setName(text)}
-          />
-        </View>
-        <View style={styles.info}>
-          <TextInput
-            style={styles.textInput}
-            placeholder="Enter Room ID"
-            placeholderTextColor="#767476"
-            value={name}
-            onChangeText={(text) => setRoomId(text)}
-          />
-        </View>
-        <View style={{ alignItems: "center" }}>
-          <TouchableOpacity style={styles.startMeetingButton}>
-            <Text style={{ color: "white", fontWeight: "bold" }}>
-              Start Meeting
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+      <StartMeeting
+        name={name}
+        setName={setName}
+        roomId={roomId}
+        setRoomId={setRoomId}
+      />
     </View>
   );
 }
@@ -52,27 +29,4 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   startMeetingContainer: {},
-  info: {
-    width: "100%",
-    backgroundColor: "#373538",
-    height: 50,
-    borderTopWidth: 1,
-    borderBottomWidth: 1,
-    borderColor: "#484648",
-    padding: 12,
-    justifyContent: "center",
-  },
-  textInput: {
-    color: "white",
-    fontSize: 18,
-  },
-  startMeetingButton: {
-    width: 350,
-    height: 50,
-    marginTop: 20,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#0470DC",
-    borderRadius: 15,
-  },
 });
