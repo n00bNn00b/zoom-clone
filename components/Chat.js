321926af880f65524d871f22b7ad8c39ed364532
n00bNn00b <reldewan9@gmail.com>
2022-07-30 15:45:30 +0600

    finalized

--- components/Chat.js
+++ components/Chat.js
@@ -1,13 +1,55 @@
-import React from "react";
-import { View, Text, StyleSheet, TextInput, SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  SafeAreaView,
+  Touchable,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+} from "react-native";
 import ChatHeader from "./ChatHeader";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+
 function Chat({ setModalVisible }) {
+  const [messageText, setMessageText] = useState("");
   return (
     <View style={styles.container}>
       <SafeAreaView style={{ height: "100%" }}>
-        <ChatHeader setModalVisible={setModalVisible} />
-        {/* chat messages */}
-        {/* type messages */}
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={styles.container}
+        >
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={{ flex: 1 }}>
+              <ChatHeader setModalVisible={setModalVisible} />
+              {/* chat messages */}
+              <View style={styles.chatMessages}></View>
+              {/* type messages */}
+              <View style={styles.chatFormContainer}>
+                <Text style={{ color: "white" }}>Send to Everyone</Text>
+                <View style={styles.chatform}>
+                  <TextInput
+                    value={messageText}
+                    onChangeText={(text) => setMessageText(text)}
+                    style={styles.textInput}
+                    placeholder="Tap here to chat"
+                    placeholderTextColor={"#595859"}
+                  />
+                  <TouchableOpacity
+                    style={{
+                      ...styles.button,
+                      backgroundColor: messageText ? "#0B71EB" : "#373838",
+                    }}
+                  >
+                    <FontAwesome name="send" size={18} color="#efefef" />
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
       </SafeAreaView>
     </View>
   );
@@ -19,5 +61,36 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#1c1c1c",
+  },
+  chatFormContainer: {
+    borderColor: "#2f2f2f",
+    borderTopWidth: 1,
+    padding: 12,
+  },
+  chatform: {
+    flexDirection: "row",
+  },
+  textInput: {
+    height: 40,
+    color: "#efefef",
+    borderColor: "#595859",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    marginTop: 12,
+    flex: 1,
+  },
+  button: {
+    height: 40,
+    width: 40,
+    marginTop: 12,
+    marginLeft: 12,
+
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  chatMessages: {
+    flex: 1,
   },
 });
